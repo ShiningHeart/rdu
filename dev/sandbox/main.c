01036843c3a14903f06cc59de65727a1a2db27de
Milos <mlazic95@htomail.com>
2018-07-03 19:10:37 -0700

    sandbox main.c and Makefile cleaned up

--- dev/sandbox/main.c
+++ dev/sandbox/main.c
@@ -10,26 +10,8 @@
 
 int main( int argc, char *argv[])
 {
-	pid_t pid;
-	int status, err;
-	char *dxargs[] = {};
 
-	printf("Hello sandbox!\n");
-
-	pid = fork();
-	if ( pid == 0) // child process
-	{
-		err = execve("../dx/dx", dxargs, NULL);
-		if ( err == -1)
-		{
-			printf("Error: execve(), %s\r\n", strerror(errno));
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	waitpid( pid, &status, 0);
-
-	printf("Goodbye sandbox\n");
+	printf("Hello, World!\n");
 
 	return 0;
 }
